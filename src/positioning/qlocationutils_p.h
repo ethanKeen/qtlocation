c8634937dd99346c6b063ece697adedda64ec39f
ethanKeen <131292312+ethanKeen@users.noreply.github.com>
2025-05-09 08:42:05 +0100

    Update qlocationutils_p.h

--- src/positioning/qlocationutils_p.h
+++ src/positioning/qlocationutils_p.h
@@ -96,7 +96,8 @@ public:
         NmeaSentenceRMC, // Recommended minimum data for gps
         NmeaSentenceVTG, // Vector track an Speed over the Ground
         NmeaSentenceZDA, // Date and Time
-        NmeaSentenceGSV  // Per-Satellite Info
+        NmeaSentenceGSV,  // Per-Satellite Info
+        NmeaSentenceHDT, // Added Absolute heading
     };
 
     inline static bool isValidLat(double lat) {
